93778b7dc45bc0f6ef77b4718c6c1160880cecf4
mar156 <m.n.v_156@hotmail.com.ar>
2020-07-08 21:09:25 -0300

    Agregué en JS las rutas de iniciar sesión y crear cuenta

--- E-commerceNODE.js
+++ E-commerceNODE.js
@@ -6,5 +6,11 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/crear-cuenta', (req,res) =>{
+    res.sendFile(__dirname + '/formulario-usuario.html');
+});
+app.get('/iniciar-sesion',(req,res) => {
+    res.sendFile(__dirname + '/iniciar-sesion.html');
+})
 
 app.listen(3000, () => {console.log('El servidor funciona bien')});
